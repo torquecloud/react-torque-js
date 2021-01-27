a40ab521c0088a7e42bca90c65829f7eedeac258
Miroslav Jelaska <miro@torque.cloud>
2021-01-27 21:02:25 +0100

    improved error handling and exposed refreshTorqueUser

--- src/torque-context.ts
+++ src/torque-context.ts
@@ -1,15 +1,24 @@
 import React, { useContext } from 'react'
-import { Torque, TorqueUser, UnknownTorqueUser } from '@torquecloud/torque-js'
+import { Torque, TorqueError, TorqueErrorType, TorqueUser, UnknownTorqueUser } from '@torquecloud/torque-js'
 
 export type TorqueContextType = {
   torque: Torque | null
-  torqueUser: TorqueUser
+  torqueUser: TorqueUser,
+  refreshTorqueUser: () => Promise<{ error?: TorqueError }>
 }
 
 export const TorqueContext =
   React.createContext<TorqueContextType>({
     torque: null,
     torqueUser: UnknownTorqueUser.Instance,
+    refreshTorqueUser: () => Promise.resolve<{ error: TorqueError }>(
+      {
+        error: {
+          type: TorqueErrorType.invalid_config,
+          message: `Missing Torque instance. Torque instance is necessary to refresh TorqueUser.`,
+        },
+      },
+    ),
   })
 
 export const useTorque =
